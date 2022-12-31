d66ae9b16e19a7464648a6b70f8920baf6fbdca5
Geriano <gerznewbie@gmail.com>
2022-12-31 16:10:02 +0700

    moving generate route into function

---
+++ lib/generate_route.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:template/main.dart';
+import 'package:template/pages/auth/login.dart';
+import 'package:template/pages/dashboard/index.dart';
+import 'package:template/pages/loading.dart';
+import 'package:template/providers/auth.dart';
+import 'package:template/requests/login.dart';
+import 'package:template/responses/login.dart';
+
+Route<dynamic>? generateRoute(AuthProvider auth, RouteSettings settings) {
+  if (auth.authenticated) {
+    return null;
+  }
+
+  return MaterialPageRoute(
+    builder: (context) => FutureBuilder(
+      initialData: false,
+      future: Future<String?>.delayed(
+        const Duration(milliseconds: 1000), 
+        () => localStorage!.getItem('token'),
+      ),
+      builder: (context, snapshot) {
+        if (snapshot.data == false) {
+          return loading;
+        }
+
+        if (snapshot.data == null) {
+          return const Login();
+        }
+        
+        return FutureBuilder(
+          future: LoginRequest().token(snapshot.data as String),
+          builder: (context, snapshot) {
+            if (snapshot.data == null) {
+              return loading;
+            }
+
+            if (snapshot.data is LoginSuccessResponse) {
+              return const Dashboard();
+            }
+
+            return const Login();
+          },
+        );
+      },
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,10 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:localstorage/localstorage.dart';
 import 'package:provider/provider.dart';
-import 'package:template/pages/auth/login.dart';
-import 'package:template/pages/dashboard/index.dart';
-import 'package:template/pages/loading.dart';
+import 'package:template/generate_route.dart';
 import 'package:template/providers.dart';
 import 'package:template/providers/auth.dart';
-import 'package:template/requests/login.dart';
-import 'package:template/responses/login.dart';
-import 'package:template/routes.dart';
 import 'package:template/theme.dart';
 
 LocalStorage? localStorage;
@@ -45,49 +38,12 @@ class TemplateState extends State<Template> {
         splitScreenMode: true,
         builder: (context, child) {
           var auth = context.watch<AuthProvider>();
-
+          
           return MaterialApp(
             title: 'Template',
             theme: theme,
             initialRoute: '/',
-            onGenerateRoute: (settings) {
-              if (auth.authenticated) {
-                return MaterialPageRoute(
-                  builder: routes[settings.name]!,
-                );
-              }
-
-              return MaterialPageRoute(
-                builder: (context) => FutureBuilder(
-                  initialData: false,
-                  future: Future<String?>.delayed(const Duration(milliseconds: 1000), () => localStorage!.getItem('token')),
-                  builder: (context, snapshot) {
-                    if (snapshot.data == false) {
-                      return loading;
-                    }
-
-                    if (snapshot.data == null) {
-                      return const Login();
-                    }
-                    
-                    return FutureBuilder(
-                      future: LoginRequest().token(snapshot.data as String),
-                      builder: (context, snapshot) {
-                        if (snapshot.data == null) {
-                          return loading;
-                        }
-
-                        if (snapshot.data is LoginSuccessResponse) {
-                          return const Dashboard();
-                        }
-
-                        return const Login();
-                      },
-                    );
-                  },
-                ),
-              );
-            },
+            onGenerateRoute: (settings) => auth.authenticated ? null : generateRoute(auth, settings,),
           );
         },
       ),
